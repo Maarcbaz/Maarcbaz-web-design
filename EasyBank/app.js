28bcb69f8f392c39ed7f9634d03621133242f48a
Maarcbaz <138406453+Maarcbaz@users.noreply.github.com>
2023-11-21 19:23:10 +0100

    created responsive navbar

--- EasyBank/app.js
+++ EasyBank/app.js
@@ -1,6 +1,7 @@
 function openNav() {
-    document.getElementById('myNav').style.width = "30%"
+  document.getElementById("myNav").style.width = "50%";
 }
+
 function closeNav() {
-    document.getElementById('myNav').style.width = '0%'
+  document.getElementById("myNav").style.width = "0%";
 }
